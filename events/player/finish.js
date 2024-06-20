67f2a4564c43fb99bee007f5ea30eea72e9cf86c
Q3nfQ <bender1423@hotmail.com>
2024-06-20 06:51:24 +0300

    Update finish.js

--- events/player/finish.js
+++ events/player/finish.js
@@ -6,21 +6,16 @@ module.exports = async (client, queue) => {
 
 /*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
+         
 
 ╔════════════════════════════════════════════════════════════════════════╗
 ║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
+║  ## Created by Bander!                                                ║
 ║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║  ## DISCORD :  https://discord.com/invite/B-B                          ║
 ║                                                                        ║
 ╚════════════════════════════════════════════════════════════════════════╝
 
 
 */
+
